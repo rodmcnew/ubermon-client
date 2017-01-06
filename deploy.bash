cd9389cd1b572ecc12fd7e69c3094c33e5e0a6ce
rodmcnew <rodmcnew@gmail.com>
2017-01-06 17:56:45 -0600

    add files

--- deploy.bash
+++ deploy.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 npm run build
 git remote add production git@github.com:ubermon-client/ubermon-client.github.io.git
-git add build && git commit -m "add build files"
+git add . && git commit -m "add files"
 git subtree push --prefix build production master
