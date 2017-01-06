0f712e20bc42faa7391a75acffa8a543d0883931
rodmcnew <rodmcnew@gmail.com>
2017-01-06 17:46:55 -0600

    add files

--- deploy.bash
+++ deploy.bash
@@ -1,3 +1,5 @@
-git remote add build git@github.com:ubermon-client/ubermon-client.github.io.git
-git add build && git commit -m "Initial build subtree commit"
-git subtree push --prefix build dist gh-pages
+#!/usr/bin/env bash
+npm run build
+git remote add production git@github.com:ubermon-client/ubermon-client.github.io.git
+git add build && git commit -m "add build files"
+git subtree push --prefix build production master --force
