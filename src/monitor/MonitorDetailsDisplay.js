cb0ba3decc61b0f4d04a9f4ab94263f3571d1f8c
Rod Mcnew <rodmcnew@gmail.com>
2017-01-06 19:42:54 -0600

    add build files

--- src/monitor/MonitorDetailsDisplay.js
+++ src/monitor/MonitorDetailsDisplay.js
@@ -19,9 +19,6 @@ export default class MonitorDetailsDisplay extends Component {
 
         return (
             <div>
-                {/*<!--<div>-->*/}
-                {/*<!--Type: <span ng-bind="monitorTypes[selectedMonitor.type]"></span>-->*/}
-                {/*<!--</div>-->*/}
                 <div>
                     Name: {monitor.name}
                 </div>
@@ -34,48 +31,11 @@ export default class MonitorDetailsDisplay extends Component {
                 <div>
                     Monitoring Interval: {this.props.monitorIntervals[monitor.interval]}
                 </div>
+                <br/>
                 <h4 style={{display: 'inline'}}>Response Time</h4> (ms)
                 <MonitorPingChart pings={this.props.pings}/>
                 <h4>Latest Events</h4>
                 <MonitorEventsDisplay events={this.props.events}/>
-                {/*<!--<div>-->*/}
-                {/*<!--Uptime: <strike>Unknown</strike>-->*/}
-                {/*<!--</div>-->*/}
-                {/*<br/>*/}
-
-                {/*<div ng-hide="selectedMonitorEvents.length > 0">*/}
-                {/*<h4>Loading...</h4>*/}
-                {/*</div>*/}
-                {/*<div ng-show="selectedMonitorEvents.length > 0">*/}
-                {/*<h4 style="display: inline">Response Time</h4> (ms)*/}
-                {/*<div ng-show="selectedMonitorPingChart.data[0]">*/}
-                {/*<canvas id="line" class="chart chart-line" width="100" height="30"*/}
-                {/*chart-data="selectedMonitorPingChart.data"*/}
-                {/*chart-labels="selectedMonitorPingChart.labels"*/}
-                {/*chart-click="onClick"*/}
-                {/*chart-options="{showXLabels: 5, showTooltips: false, bezierCurve : false, animation: false}">*/}
-                {/*</canvas>*/}
-                {/*</div>*/}
-                {/*<h4>Latest Events</h4>*/}
-                {/*<table class="table">*/}
-                {/*<tr>*/}
-                {/*<th>Status</th>*/}
-                {/*<th>Date</th>*/}
-                {/*<th>Reason</th>*/}
-                {/*<!--<th>Duration</th>-->*/}
-                {/*</tr>*/}
-                {/*<tr ng-repeat="event in selectedMonitorEvents">*/}
-                {/*<td>*/}
-                {/*<span ng-show="event.type=='u'">Up</span>*/}
-                {/*<span ng-show="event.type=='d'">Down</span>*/}
-                {/*<span ng-show="event.type=='p'">Paused</span>*/}
-                {/*<span ng-show="event.type=='s'">Started</span>*/}
-                {/*</td>*/}
-                {/*<td ng-bind="event.date"></td>*/}
-                {/*<td ng-bind="event.reason"></td>*/}
-                {/*<!--<td><strike>Unknown</strike></td>-->*/}
-                {/*</tr>*/}
-                {/*</table>*/}
             </div>
         )
     }
