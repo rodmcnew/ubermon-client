ae609f06422c35a0bfcd8be9b0729869bd6fa62a
rodmcnew <rodmcnew@gmail.com>
2017-02-01 20:01:49 -0600

    add build files

--- deploy.bash
+++ deploy.bash
@@ -3,4 +3,4 @@ npm run build
 git remote add production git@github.com:ubermon-client-react-redux/ubermon-client-react-redux.github.io.git
 git add . && git commit -m "add build files"
 git push origin master
-git subtree push --prefix build production master
+git push production `git subtree split --prefix build master`:master --force
