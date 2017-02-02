60fe0fad164a0ae3940ab7cebaca58b4052f08dd
rodmcnew <rodmcnew@gmail.com>
2017-02-02 14:40:03 -0600

    add build files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import 'babel-polyfill';
+import 'babel-polyfill';//Makes things like array.find work in IE11
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
